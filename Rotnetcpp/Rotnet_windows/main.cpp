146bce3c480062eb4e1389ac738fbb85adec21cc
Jacques Figuinha <jfiguinha@hotmail.fr>
2020-07-12 17:54:14 +0200

    Update project with facial recognition

--- Rotnetcpp/Rotnet_windows/main.cpp
+++ Rotnetcpp/Rotnet_windows/main.cpp
@@ -1,21 +1,163 @@
 #include <fdeep/fdeep.hpp>
 #include <opencv2/opencv.hpp>
+#include <opencv2/dnn.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/core.hpp>
+#include <string>
+using namespace cv;
+using namespace std;
+using namespace cv::dnn;
+#define CAFFE
+
+using namespace std;
+
+int RotateToExifOrientation(const int &angle)
+{
+	if (angle < 45 && angle > 314)
+		return 0;
+	else if (angle > 44 && angle < 135) //Rotate 90
+		return 6;
+	else if (angle > 134 && angle < 225) //Rotate 180
+		return 3;
+	else
+		return 8;
+	return 0;
+}
+
+int RotateToOrientation(const int &angle)
+{
+	if (angle < 45 && angle > 314)
+		return 0;
+	else if (angle > 44 && angle < 135) //Rotate 90
+		return 90;
+	else if (angle > 134 && angle < 225) //Rotate 180
+		return 180;
+	else
+		return 270;
+	return 0;
+}
+
+int DeterminePictureAngle(cv::Mat & image)
+{
+	cv::Size size(224, 224);//the dst image size,e.g.100x100
+	cv::Mat dst;//dst image
+	cv::resize(image, dst, size);
+	assert(image.isContinuous());
+	const auto model = fdeep::load_model("C:\\developpement\\git_gcc\\Rotnet\\Rotnetcpp\\model\\rotnet_street_view_resnet50_keras2.json");
+	// Use the correct scaling, i.e., low and high.
+	const auto input = fdeep::tensor_from_bytes(dst.ptr(),
+		static_cast<std::size_t>(dst.rows),
+		static_cast<std::size_t>(dst.cols),
+		static_cast<std::size_t>(dst.channels()),
+		0.0f, 1.0f);
+	return model.predict_class({ input });
+}
+
+const size_t inWidth = 300;
+const size_t inHeight = 300;
+const double inScaleFactor = 1.0;
+const float confidenceThreshold = 0.7;
+const cv::Scalar meanVal(104.0, 177.0, 123.0);
+
+//--------------------------------------------------
+//Code From https://github.com/spmallick/learnopencv
+//--------------------------------------------------
+void detectFaceOpenCVDNN(Net net, Mat &frameOpenCVDNN)
+{
+	int frameHeight = frameOpenCVDNN.rows;
+	int frameWidth = frameOpenCVDNN.cols;
+#ifdef CAFFE
+	cv::Mat inputBlob = cv::dnn::blobFromImage(frameOpenCVDNN, inScaleFactor, cv::Size(inWidth, inHeight), meanVal, false, false);
+#else
+	cv::Mat inputBlob = cv::dnn::blobFromImage(frameOpenCVDNN, inScaleFactor, cv::Size(inWidth, inHeight), meanVal, true, false);
+#endif
+
+	net.setInput(inputBlob, "data");
+	cv::Mat detection = net.forward("detection_out");
+
+	cv::Mat detectionMat(detection.size[2], detection.size[3], CV_32F, detection.ptr<float>());
+
+	try
+	{
+		for (int i = 0; i < detectionMat.rows; i++)
+		{
+			float confidence = detectionMat.at<float>(i, 2);
+
+			if (confidence > confidenceThreshold)
+			{
+				int x1 = static_cast<int>(detectionMat.at<float>(i, 3) * frameWidth);
+				int y1 = static_cast<int>(detectionMat.at<float>(i, 4) * frameHeight);
+				int x2 = static_cast<int>(detectionMat.at<float>(i, 5) * frameWidth);
+				int y2 = static_cast<int>(detectionMat.at<float>(i, 6) * frameHeight);
+
+				cv::rectangle(frameOpenCVDNN, cv::Point(x1, y1), cv::Point(x2, y2), cv::Scalar(0, 255, 0), 22, 4);
+			}
+		}
+	}
+	catch (cv::Exception& e)
+	{
+		const char* err_msg = e.what();
+		std::cout << "exception caught: " << err_msg << std::endl;
+		std::cout << "wrong file format, please input the name of an IMAGE file" << std::endl;
+	}
+}
+
+void FaceDetection(cv::Mat & image)
+{
+
+
+	const std::string caffeConfigFile = "C:\\developpement\\git_gcc\\Rotnet\\Rotnetcpp\\model\\deploy.prototxt";
+	const std::string caffeWeightFile = "C:\\developpement\\git_gcc\\Rotnet\\Rotnetcpp\\model\\res10_300x300_ssd_iter_140000_fp16.caffemodel";
+	const std::string tensorflowConfigFile = "C:\\developpement\\git_gcc\\Rotnet\\Rotnetcpp\\model\\opencv_face_detector.pbtxt";
+	const std::string tensorflowWeightFile = "C:\\developpement\\git_gcc\\Rotnet\\Rotnetcpp\\model\\opencv_face_detector_uint8.pb";
+	Net net;
+
+	try
+	{
+#ifdef CAFFE
+		net = cv::dnn::readNetFromCaffe(caffeConfigFile, caffeWeightFile);
+#else
+		net = cv::dnn::readNetFromTensorflow(tensorflowWeightFile, tensorflowConfigFile);
+#endif
+	}
+	catch (cv::Exception& e)
+	{
+		const char* err_msg = e.what();
+		std::cout << "exception caught: " << err_msg << std::endl;
+		std::cout << "wrong file format, please input the name of an IMAGE file" << std::endl;
+	}
+
+
+	detectFaceOpenCVDNN(net, image);
+}
 
 int main()
 {
-    cv::Size size(224,224);//the dst image size,e.g.100x100
-    cv::Mat dst;//dst image
-    const cv::Mat image = cv::imread("/home/figuinha/developpement/picture/photo.jpg");
-    cv::cvtColor(image, image, cv::COLOR_BGR2RGB);
-    cv::resize(image,dst,size);
-    assert(image.isContinuous());
-    const auto model = fdeep::load_model("model/rotnet_street_view_resnet50_keras2.json");
-    // Use the correct scaling, i.e., low and high.
-    const auto input = fdeep::tensor_from_bytes(dst.ptr(),
-        static_cast<std::size_t>(dst.rows),
-        static_cast<std::size_t>(dst.cols),
-        static_cast<std::size_t>(dst.channels()),
-        0.0f, 1.0f);
-    const auto result = model.predict_class({input});
-    std::cout << result << std::endl;
+	cv::Mat image = cv::imread("D:\\photos\\test\\photo.jpg");
+	//cv::cvtColor(image, image, cv::COLOR_BGR2RGB);
+	
+	int angle = RotateToOrientation(DeterminePictureAngle(image));
+	std::cout <<  "Exif orientation : " << RotateToExifOrientation(angle) << std::endl;
+	//imshow("Source", image);
+	char k = waitKey();
+	// get rotation matrix for rotating the image around its center in pixel coordinates
+	cv::Point2f center((image.cols - 1) / 2.0, (image.rows - 1) / 2.0);
+	cv::Mat rot = cv::getRotationMatrix2D(center, angle, 1.0);
+	// determine bounding rectangle, center not relevant
+	cv::Rect2f bbox = cv::RotatedRect(cv::Point2f(), image.size(), angle).boundingRect2f();
+	// adjust transformation matrix
+	rot.at<double>(0, 2) += bbox.width / 2.0 - image.cols / 2.0;
+	rot.at<double>(1, 2) += bbox.height / 2.0 - image.rows / 2.0;
+
+	cv::Mat dst;
+	cv::warpAffine(image, dst, rot, bbox.size());
+	//cv::imwrite("rotated_im.png", dst);
+	//imshow("Rotate", dst);
+
+	FaceDetection(dst);
+	k = waitKey();
+    std::cout << angle << std::endl;
+
+	cv::imwrite("D:\\photos\\test\\photo_out.jpg", dst);
 }
